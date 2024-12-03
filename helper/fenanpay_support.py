2e7a8ead44b018c8a074bce5cc2642528a1ad6bd
bereket-7 <bekhonelign@gmail.com>
2024-12-04 00:01:27 +0300

    fenanpay support

--- helper/fenanpay_support.py
+++ helper/fenanpay_support.py
@@ -1,48 +1,60 @@
-from datetime import datetime
 import json
 import requests
-from exception import (
-    fenanpay_bad_request_exception,
-    fenanpay_network_exception,
-    fenanpay_not_found_exception,
-    fenanpay_unauthorized_exception,
-    fenanpay_exception
-)
+from datetime import datetime
+import logging
+
+from exception.fenanpay_bad_request_exception import FenanpayBadRequestException
+from exception.fenanpay_exception import FenanpayException
+from exception.fenanpay_network_exception import FenanpayNetworkException
+from exception.fenanpay_not_found_exception import FenanpayNotFoundException
+from exception.fenanpay_unauthorized_exception import FenanpayUnauthorizedException
+
+logger = logging.getLogger(__name__)
 
 
 class FenanpaySupport:
-
     @staticmethod
     def get_expire_date_from_date(date: datetime) -> str:
-        # Assuming date is a datetime object
         return date.strftime('%Y-%m-%dT%H:%M:%S')
 
     @staticmethod
-    def handle_exception(e: requests.RequestException):
-        if isinstance(e, requests.HTTPError):
+    def handle_exception(e: requests.exceptions.RequestException):
+        if isinstance(e, requests.exceptions.HTTPError):
             response = e.response
             if response:
-                if response.status_code == 401:
-                    raise fenanpay_unauthorized_exception(
+                status_code = response.status_code
+                response_body = response.text
+
+                if status_code == 401:
+                    logger.error('Invalid authentication credentials')
+                    raise FenanpayUnauthorizedException(
                         'Invalid authentication credentials') from e
-                elif response.status_code == 400:
+                elif status_code == 400:
                     try:
                         response_json = response.json()
                         msg = response_json.get(
-                            "msg", "Invalid Request, check your Request body.")
+                            'msg', 'Invalid Request, check your Request body.')
                     except json.JSONDecodeError:
-                        msg = "Invalid Request, check your Request body."
-                    raise fenanpay_bad_request_exception(msg) from e
-                elif response.status_code == 404:
+                        msg = 'Invalid Request, check your Request body.'
+                    logger.error(msg)
+                    raise FenanpayBadRequestException(msg) from e
+                elif status_code == 404:
                     try:
                         response_json = response.json()
                         msg = response_json.get(
-                            "msg", "Invalid Request, Not found.")
+                            'msg', 'Invalid Request, Not found.')
                     except json.JSONDecodeError:
-                        msg = "Invalid Request, Not found."
-                    raise fenanpay_not_found_exception(msg) from e
+                        msg = 'Invalid Request, Not found.'
+                    logger.error(msg)
+                    raise FenanpayNotFoundException(msg) from e
                 else:
-                    msg = response.json().get("msg", "An error occurred.")
-                    raise fenanpay_exception(msg) from e
+                    try:
+                        response_json = response.json()
+                        msg = response_json.get('msg', 'An error occurred.')
+                    except json.JSONDecodeError:
+                        msg = 'An error occurred.'
+                    logger.error(msg)
+                    raise FenanpayException(msg) from e
         else:
-            raise fenanpay_network_exception("Network error occurred") from e
+            logger.error('A network error occurred')
+            raise FenanpayNetworkException('A network error occurred') from e
